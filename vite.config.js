45171ae9f85a78ed50e5eda5d1189e2111a60293
Ravee <148472408+RaviRepo99@users.noreply.github.com>
2026-03-13 15:20:35 +0545

    fix github pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/madhesh-matters-frontend/',
+
   server: {
-    host: '0.0.0.0', // Allow external connections
-    port: 5173, // Default Vite port
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // Your Flask backend address
+        target: 'http://localhost:8000',
         changeOrigin: true,
         secure: false,
       },
@@ -19,6 +20,6 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       }
-    },
-  },
+    }
+  }
 })
